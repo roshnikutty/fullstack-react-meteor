78bc173643641d7e8357fab0301070303cf57921
Roshni Kutty <roshni.ajkutty@gmail.com>
2018-02-22 10:26:21 -0500

    lodash & faker used to add 50000 fake employees on server

---
+++ server/main.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';                         //utilities library
+import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
+
+Meteor.startup(() => {
+    //generate data after Meteor starts on server
+    //check to see if data exists in the collection so that if it does, 
+    //do not load new data
+    const numberRecords = Employees.find({}).count();  //check everything that are in the collection, so use {}
+    if (!numberRecords) {
+        //callback is called 5000 times- used instead of a for-loop
+        _.times(5000, () => {
+            //createCard method generates a profile for a fake person
+            const { name, email, phone } = helpers.createCard();
+
+            //using values from above, insert new employee in the collection using meteor's mongo insert method
+            Employees.insert({
+                name,
+                email,
+                phone,
+                avatar: image.avatar()
+            });
+        });
+    }
+});
